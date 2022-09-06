6138b79a4cf525ad83289821a346676fcb16de06
ykgod <kyum0401@gmail.com>
2022-09-06 14:43:05 +0900

    mytoken 기초 틀완성

--- Server/routes/mytoken.js
+++ Server/routes/mytoken.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const controller = require("../controllers/token.controller");
 
 router.post("/tokensave", controller.savetoken);
-router.get("/gettoken", controller.gettoken);
+router.get("/", controller.gettoken);
 router.post("/transfer", controller.transfer);
 module.exports = router;
