f35a655e91ac797055794db37147a729dcfa0461
hongyu <yuuuj72@gmail.com>
2022-09-07 04:14:47 +0900

    staking page/component final

--- client/src/components/staking/create.js
+++ client/src/components/staking/create.js
@@ -1,8 +1,133 @@
+import React, { useState } from "react";
+import { Button, Modal, Form, Tab, Tabs } from 'react-bootstrap';
+
 function Create() {
+
+    const [key, setKey] = useState('klay');
+
+    const [create, setCreate] = useState(false);
+
+	const handleCreate = () => setCreate(true);
+	const createClose = () => setCreate(false);
+
     return (
-        <div>
-            <p>Create</p>
+        <div className="createbtn">
             
+
+
+
+            <div className="d-grid gap-2">
+                <Button variant="primary" size="lg" onClick={handleCreate}>
+                    Create New Pool
+                </Button>
+                <Modal
+                    size="lg"
+                    show={create}
+                    onHide={createClose}
+                    backdrop="static"
+                    keyboard={false}
+                    aria-labelledby="example-modal-sizes-title-sm"
+                >
+                    <Modal.Header closeButton>
+                    <Modal.Title>Create New Pool</Modal.Title>
+                    </Modal.Header>
+                    <Tabs
+                        id="controlled-tab-example"
+                        activeKey={key}
+                        onSelect={(k) => setKey(k)}
+                        className="mb-3"
+                        >
+                        <Tab eventKey="klay" title="Klay">
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group className="mb-3">
+                                <Form.Label>Klay</Form.Label>
+                                    <Form.Select disabled >
+                                    <option>Klay</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Select Token1</Form.Label>
+                                    <Form.Select >
+                                    {/* 토큰 종류 추가시 등록 */}
+                                    <option>토큰1</option>
+                                    <option>토큰2</option>
+                                    <option>[다른토큰들]</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                    <Form.Label>Token Name</Form.Label>
+                                    <Form.Control
+                                    type="text"
+                                    placeholder="EX) ETH"
+                                    autoFocus
+                                    />
+                                </Form.Group>
+                                <Form.Group
+                                    className="mb-3"
+                                    controlId="exampleForm.ControlTextarea1"
+                                >
+                                    <Form.Label>Price</Form.Label>
+                                    <Form.Control as="textarea" rows={1} />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={createClose}>
+                            취소하기
+                        </Button>
+                        <Button type="submit" variant="primary">Create</Button>
+                        </Modal.Footer>
+                        </Tab>
+
+                        <Tab eventKey="other" title="Other">
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Select Token1</Form.Label>
+                                    <Form.Select >
+                                    {/* 토큰 종류 추가시 등록 */}
+                                    <option>토큰1</option>
+                                    <option>토큰2</option>
+                                    <option>[다른토큰들]</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Select Token2</Form.Label>
+                                    <Form.Select >
+                                    {/* 토큰 종류 추가시 등록 */}
+                                    <option>Klay</option>
+                                    <option>[다른토큰들]</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                    <Form.Label>Token Name</Form.Label>
+                                    <Form.Control
+                                    type="text"
+                                    placeholder="EX) ETH"
+                                    autoFocus
+                                    />
+                                </Form.Group>
+                                <Form.Group
+                                    className="mb-3"
+                                    controlId="exampleForm.ControlTextarea1"
+                                >
+                                    <Form.Label>Price</Form.Label>
+                                    <Form.Control as="textarea" rows={1} />
+                                </Form.Group>
+                        </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                        <Button variant="secondary" onClick={createClose}>
+                            취소하기
+                        </Button>
+                        <Button type="submit" variant="primary">Create</Button>
+                        </Modal.Footer>
+                        </Tab>
+                    </Tabs>
+                    
+                </Modal>
+            </div>
         </div>
     );
 }
--- client/src/components/staking/single.js
+++ client/src/components/staking/single.js
@@ -1,8 +1,154 @@
+import { useState } from "react";
+import { Card, Row, Col, Button, Modal, Form, InputGroup } from 'react-bootstrap';
+import Create from "./create";
+
 function Single() {
+    const [depo, setDeposit] = useState(false);
+    const [widr, setWithdraw] = useState(false);
+    // const [show, setShow] = useState(false);
+
+    const depositShow = () => setDeposit(true);
+	const depositClose = () => setDeposit(false);
+
+    const withdrawShow = () => setWithdraw(true);
+	const withdrawClose = () => setWithdraw(false);
+
+    // const handleShow = () => setShow(true);
+	// const handleClose = () => setShow(false);
+      
     return (
         <div>
             <h1>Single Pool List</h1>
-            
+            <Row xs={1} md={1} className="g-4">
+            {Array.from({ length: 5 }).map((_, idx) => (
+                <Col>
+                <Card
+                    bg={'Secondary'}
+                    key={'Secondary'}
+                    text={'dark'}
+                    border={'Secondary'}
+                    style={{ width: '50rem' }}
+                    className="mb-2" 
+                >
+                    <Card.Body>
+                    <Card.Title>Token Name</Card.Title>
+                    <Card.Text>
+                        <p>총 예치규모</p>
+                        <p>내 보유량</p>
+                    </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <>
+                            <Button variant="primary" onClick={depositShow} >Deposit</Button>
+                            {/* <Button variant="primary" onClick={withdrawShow}>Withdraw</Button> */}
+                            <Modal
+                                size="lg"
+                                show={depo}
+                                onHide={depositClose}
+                                backdrop="static"
+                                keyboard={false}
+                                aria-labelledby="example-modal-sizes-title-sm"
+                            >
+                                <Modal.Header closeButton>
+                                {/* 선택한 카드의 풀 이름과 맵핑 */}
+                                <Modal.Title>[토큰이름] Deposit</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <div>  
+                                        <h5>내 예치 자산</h5>
+                                        <strong>0{/*[예치한토큰갯수]*/}</strong>
+                                        <span>{/*[토큰심볼]*/}</span>
+                                        <br/>
+                                        <br/>                         
+                                        <h5>내 지분</h5>
+                                        <strong>[보유지분율]</strong>
+                                        <span>%</span>
+                                        <br/>
+                                        <br/> 
+                                    </div>
+                                    <Form>
+                                        {/* Deposit Input  */}
+                                        {/* 토큰 이름, 심볼, 매핑 필요  */}
+                                        <Form.Label>Token Name</Form.Label> 
+                                        <InputGroup className="mb-3">
+                                            <Form.Control 
+                                                type="text"
+                                                placeholder="예치할 토큰 수량"
+                                                autoFocus
+                                                aria-label="Default"
+                                                aria-describedby="inputGroup-sizing-default"
+                                            />
+                                            <InputGroup.Text id="inputGroup-sizing-default">KLAY[토큰심볼]</InputGroup.Text>
+                                        </InputGroup>
+                                    </Form>
+                                </Modal.Body>
+                                <Modal.Footer>
+                                <Button variant="secondary" onClick={depositClose}>
+                                    취소
+                                </Button>
+                                <Button type="submit" variant="primary">확인</Button>
+                                </Modal.Footer>
+                            </Modal>
+
+                            <Button variant="primary" onClick={withdrawShow}>Withdraw</Button>
+                            <Modal
+                                size="lg"
+                                show={widr}
+                                onHide={withdrawClose}
+                                backdrop="static"
+                                keyboard={false}
+                                aria-labelledby="example-modal-sizes-title-sm"
+                            >
+                                <Modal.Header closeButton>
+                                {/* 선택한 카드의 풀 이름과 맵핑 */}
+                                <Modal.Title>[토큰이름] Withdraw</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <div>  
+                                        <h5>내 예치 자산</h5>
+                                        <strong>0{/*[예치한토큰갯수]*/}</strong>
+                                        <span>{/*[토큰심볼]*/}</span>
+                                        <br/>
+                                        <br/>                         
+                                        <h5>내 지분</h5>
+                                        <strong>[보유지분율]</strong>
+                                        <span>%</span>
+                                        <br/>
+                                        <br/> 
+                                    </div>
+                                    <Form>
+                                        {/* Withdraw Input  */}
+                                        {/* 토큰 이름, 심볼, 매핑 필요  */}
+                                        <Form.Label>Token Name</Form.Label> 
+                                        <InputGroup className="mb-3">
+                                            <Form.Control 
+                                                type="text"
+                                                placeholder="출금할 토큰 수량"
+                                                autoFocus
+                                                aria-label="Default"
+                                                aria-describedby="inputGroup-sizing-default"
+                                            />
+                                            <InputGroup.Text id="inputGroup-sizing-default">KLAY[토큰심볼]</InputGroup.Text>
+                                        </InputGroup>
+                                    </Form>
+                                </Modal.Body>
+
+                                <Modal.Footer>
+                                <Button variant="secondary" onClick={withdrawClose}>
+                                    취소
+                                </Button>
+                                <Button type="submit" variant="primary">확인</Button>
+                                </Modal.Footer>
+                            </Modal>      
+                        </>
+                    </Card.Footer>
+                </Card>
+                </Col>
+            ))}
+            </Row>
+
+            <Create />
+
         </div>
     );
 }
