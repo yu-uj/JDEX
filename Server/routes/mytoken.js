44374ff8fedb1332c9d690f23a9345cae79e7bce
hongyu <99964401+yu-uj@users.noreply.github.com>
2022-09-29 10:11:17 +0900

    Delete Server directory

--- Server/routes/mytoken.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/token.controller");
-
-router.post("/tokensave", controller.savetoken);
-router.get("/", controller.gettoken);
-router.post("/transfer", controller.transfer);
-module.exports = router;
